570b5514a0d7a3432e7560fba0610ad0425fc3ef
Tiffceet <zkang.loo@gmail.com>
2021-05-14 12:52:43 +0800

    Add: /api/auth/register endpoint

---
+++ api/src/APIS/interface/auth/RegisterResponse.ts
@@ -0,0 +1,4 @@
+export interface RegisterResponse {
+    status: boolean;
+    message?: string;
+}
---
+++ api/src/APIS/interface/firebase/AppUser.ts
@@ -0,0 +1,5 @@
+export interface AppUser {
+    username: string,
+    password: string,
+    created_time: number
+}
