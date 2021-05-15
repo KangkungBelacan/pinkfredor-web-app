84c153269872a72a945500453291b527be6424d0
Edmund <12859874+lightcmd@users.noreply.github.com>
2021-05-15 18:50:54 +0800

    Add: Music Player Test Update. Playing audio from links now work.

--- src/components/MainApp/MusicPlayer.tsx
+++ src/components/MainApp/MusicPlayer.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import Sound, { ReactSoundProps } from 'react-sound';
+import "./../../pages/MainApp.css"
 
 function MusicPlayer() {
   const [status, setStatus] = useState<ReactSoundProps['playStatus']>('STOPPED');
+  const [url, setUrl] = useState('null')
+  const [playingUrl, setPlayingUrl] = useState('null')
 
   function togglePlayStatus() {
-    setStatus(status => status === 'PAUSED' ? 'PLAYING' : 'PAUSED')
+    setStatus(status => status === 'PLAYING' ? 'PAUSED' : 'PLAYING')
   }
 
   function stopPlaying() {
@@ -22,14 +25,38 @@ function MusicPlayer() {
         return 'STOP';
     }
   }
+
     return (
-      <div>
-        <Sound
-            url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3"
-            playStatus={status}
-          />
-        <button onClick={togglePlayStatus}>Play/Pause</button>
-        <button onClick={stopPlaying}>Stop</button>
+      <div style={{
+        position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'
+          }}>
+        <div>
+          <Sound
+              url={playingUrl}
+              playStatus={status}
+            />
+          <form>
+            <p>URL To play music from:</p>
+            <input
+              type="text"
+              onChange={event => setUrl(event.target.value)}
+            />
+          <button style={{
+            margin: '15px'
+              }} className="button"
+              onClick={() => setPlayingUrl(url)}
+              type="button">
+              Confirm
+            </button>
+          </form>
+        </div>
+        <div>
+          <button className="button" onClick={togglePlayStatus}>
+            {(status == 'PLAYING') ? "Pause" : "Play"}
+          </button>
+          <button className="button" onClick={stopPlaying}>Stop</button>
+        </div>
       </div>
     );
 }
--- src/pages/MainApp.tsx
+++ src/pages/MainApp.tsx
@@ -3,8 +3,17 @@ import "./MainApp.css"
 
 function MainApp() {
     return (
-        <div className="home-body">
+        <div className="mainapp-body">
+          <h1 className="mainapp-h1">Music Playing from URL test</h1>
             <MainAppComponent.MusicPlayer />
+          <h2 className="mainapp-h2">Notes:</h2>
+          <h5 className="mainapp-h5">This is just me testing out playing audio from links. I will work on the UI later. This hardly counts as a UI.</h5>
+          <h5 className="mainapp-h5">I'm not sure if we'll be streaming audio from links.</h5>
+          <h5 className="mainapp-h5">Copy these links into the input box then press confirm to test it out:</h5>
+          <p className="mainapp-h5">https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3</p>
+          <p className="mainapp-h5">https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3</p>
+          <p className="mainapp-h5">https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3</p>
+          <p className="mainapp-h5">MP3 sources: https://www.soundhelix.com/audio-examples</p>
         </div>
     );
 }
