0d128e7fc335a77a899322f7d67d24863e8b979d
Tiffceet <zkang.loo@gmail.com>
2021-05-16 18:03:19 +0800

    API: Grant correct scope for Drive API

--- api/src/APIS/driveapi/authurl.ts
+++ api/src/APIS/driveapi/authurl.ts
@@ -8,7 +8,7 @@ const oauth2Client = new google.auth.OAuth2(
 );
 
 // generate a url that asks permissions for Blogger and Google Calendar scopes
-const scopes = ["https://www.googleapis.com/auth/drive.file"];
+const scopes = ["https://www.googleapis.com/auth/drive.appdata", "https://www.googleapis.com/auth/drive"];
 
 const authurl = (req: any, res: any) => {
     let status = verifyIncomingRequest(req);
--- api/src/APIS/driveapi/oauth_callback.ts
+++ api/src/APIS/driveapi/oauth_callback.ts
@@ -1,6 +1,6 @@
 import { google } from "googleapis";
 import drive_cred from "./../../drive_credentials.json";
-
+import {db} from "./../../firebase";
 const oauth2Client = new google.auth.OAuth2(
     (drive_cred as any).client_id,
     (drive_cred as any).client_secret,
@@ -11,7 +11,7 @@ const oauth_callback = async (req: any, res: any) => {
     let auth_code: string = req.query.code;
     let userid: string = req.query.state;
     const {tokens} = await oauth2Client.getToken(auth_code);
-    console.log(tokens);
+    db.collection("drive-api-tokens").doc(userid).set(tokens);
     res.redirect("http://localhost:3000");
 };
 
