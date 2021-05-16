4f4eeb8bb65c6882e3bde0aac9f6334b35685336
Edmund <12859874+lightcmd@users.noreply.github.com>
2021-05-16 11:38:25 +0800

    MainApp: Added proper 4 space indenting and comments for readability.

--- src/components/MainApp/MusicPlayer.tsx
+++ src/components/MainApp/MusicPlayer.tsx
@@ -1,63 +1,59 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Sound, { ReactSoundProps } from 'react-sound';
 import "./../../pages/MainApp.css"
 
+//Music Player Component.
 function MusicPlayer() {
-  const [status, setStatus] = useState<ReactSoundProps['playStatus']>('STOPPED');
-  const [url, setUrl] = useState('null')
-  const [playingUrl, setPlayingUrl] = useState('null')
+    //Control whether the song is playing or not.
+    const [status, setStatus] = useState<ReactSoundProps['playStatus']>('STOPPED');
+    //User textbox input.
+    const [input, setInput] = useState('null')
+    //URL of the song currently playing.
+    const [playingUrl, setPlayingUrl] = useState('null')
 
-  function togglePlayStatus() {
-    setStatus(status => status === 'PLAYING' ? 'PAUSED' : 'PLAYING')
-  }
-
-  function stopPlaying() {
-    setStatus(status => status = 'STOPPED')
-  }
-
-  function statusLabel(status: ReactSoundProps['playStatus']): string {
-    switch(status) {
-      case 'STOPPED':
-        return 'PLAY';
-      case 'PLAYING':
-        return 'STOP';
-      default:
-        return 'STOP';
+    //Toggle song playing status.
+    function togglePlayStatus() {
+        setStatus(status === 'PLAYING' ? 'PAUSED' : 'PLAYING')
     }
-  }
+
+    //Stop playing the song.
+    function stopPlaying() {
+        setStatus("STOPPED")
+    }
 
     return (
-      <div style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-          }}>
-        <div>
-          <Sound
-              url={playingUrl}
-              playStatus={status}
-            />
-          <form>
-            <p>URL To play music from:</p>
-            <input
-              type="text"
-              onChange={event => setUrl(event.target.value)}
-            />
-          <button style={{
-            margin: '15px'
-              }} className="button"
-              onClick={() => setPlayingUrl(url)}
-              type="button">
-              Confirm
+        <div style={{
+            position: 'absolute', left: '50%', top: '50%',
+            transform: 'translate(-50%, -50%)'
+        }}>
+            <div>
+                {/*Sound module*/}
+                <Sound
+                    url={playingUrl}
+                    playStatus={status}
+                />
+                <form>
+                    <p>URL To play music from:</p>
+                    <input
+                        type="text"
+                        onChange={event => setInput(event.target.value)}
+                    />
+                    <button style={{
+                        margin: '15px'
+                    }} className="button"
+                        onClick={() => setPlayingUrl(input)}
+                        type="button">
+                        Confirm
             </button>
-          </form>
+                </form>
+            </div>
+            <div>
+                <button className="button" onClick={togglePlayStatus}>
+                    {(status == 'PLAYING') ? "Pause" : "Play"}
+                </button>
+                <button className="button" onClick={stopPlaying}>Stop</button>
+            </div>
         </div>
-        <div>
-          <button className="button" onClick={togglePlayStatus}>
-            {(status == 'PLAYING') ? "Pause" : "Play"}
-          </button>
-          <button className="button" onClick={stopPlaying}>Stop</button>
-        </div>
-      </div>
     );
 }
 
