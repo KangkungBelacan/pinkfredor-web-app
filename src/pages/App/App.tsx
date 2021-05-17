84d7c28c4d109d2d5b5e2bc75443c125a584719e
BenjaminThio <84172516+BenjaminThio@users.noreply.github.com>
2021-05-17 17:05:48 +0800

    Help: Nav Bar

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -7,7 +7,12 @@ function App() {
     return (
         <div className='mainapp-body'>
             <div>
-                <NavBar />
+                <nav className="tr-nav-bar-container">
+                    <a href="http://localhost:3000/" className="tr-nav-bar-items-container">Home</a>
+                    <a href="http://localhost:3000/app" className="tr-nav-bar-items-container">Help</a>
+                    <a href="" className="tr-nav-bar-items-container">About</a>
+                    <div className="tr-nav-bar-items-container">Log Out</div>
+                </nav>
                 <h1 className='mainapp-h1'>Music Player Test</h1>
             </div>
             <div>
