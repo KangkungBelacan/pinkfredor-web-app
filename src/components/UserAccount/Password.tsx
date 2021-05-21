dc82bbc690101a1ccca1e17ca41f6e93ba289831
BenjaminThio <84172516+BenjaminThio@users.noreply.github.com>
2021-05-21 23:58:28 +0800

    Help Page:Fix Bugs & Change UI Design!

--- src/components/UserAccount/Password.tsx
+++ src/components/UserAccount/Password.tsx
@@ -1,23 +1,23 @@
 import "./../../pages/UserAccount";
-
+import React, { useState } from 'react';
 
 const Password = (props: any) => {
-    function invisible(event: any) {
-        var x = event.currentTarget.nextElementSibling;
-        if (x.type === "password") {
-          x.type = "text";
-        } else {
-          x.type = "password";
-        }
+    function visibility(event: any) {
+      var x = event.currentTarget.nextElementSibling;
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
       }
+    }
     return (
         <div>
             <h2>Change your password</h2>
             <label className="Label" htmlFor="CurrentPass">Current Password</label>
-            <button onClick={invisible}>Show</button>
+            <button onClick={visibility}>Show</button>
             <input id="CurrentPass" type="text" placeholder="Current Password"></input>
             <label className="Label" htmlFor="NewPass">New Password</label>
-            <button onClick={invisible}>Show</button>
+            <button onClick={visibility}>Show</button>
             <input id="NewPass" type="text" placeholder="New Password"></input>
             <br/>
             <button>UPDATE</button>
--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -4,33 +4,29 @@ import "./Help.css";
 
 function Help() {
     return (
-        <div className="help-body">
-            <nav className="help-tr-nav-bar-container">
-                <a href="http://localhost:3000/" className="help-tr-nav-bar-items-container">Home</a>
-                <a href="http://localhost:3000/app" className="help-tr-nav-bar-items-container">Main App</a>
-                <a href="" className="help-tr-nav-bar-items-container">About</a>
-                <div className="help-tr-nav-bar-items-container">Log Out</div>
+        <div>
+            <nav className="tr-nav-bar-container">
+                <a href="http://localhost:3000/" className="tr-nav-bar-items-container">Home</a>
+                <a href="http://localhost:3000/app" className="tr-nav-bar-items-container">Main App</a>
+                <a href="" className="tr-nav-bar-items-container">About</a>
+                <div className="tr-nav-bar-items-container">Log Out</div>
             </nav>
-            <h2 className="GlowBig">Help Centre</h2>
-            <p className="GlowSmall">Welcome To Help Centre</p>
-            <br />
-            <img src={app_img} alt="App_Image" className="CenterImage"></img>
-            <br />
-            <p className="GlowMedium">『Articles Related』</p>
-            <p className="CommonText">『Introduction』</p>
+            <h1>Help</h1>
+            <h2>Welcome To Help Centre!</h2>
+            <p>Introduction</p>
             <HelpComponent.CollapsibleText title="What can i do?" content="I used to streams your music collections from Google Drive!"/>
-            <p className="CommonText">『User』</p>
+            <p>User</p>
             <HelpComponent.CollapsibleText title="How to sign up" content="There will be an answer when the whole website has done!"/>
             <HelpComponent.CollapsibleText title="How to log out" content="There will be an answer when the whole website has done!"/>
-            <p className="CommonText">『Music』</p>
+            <p>Music</p>
             <HelpComponent.CollapsibleText title="How to play a music" content="There will be an answer when the whole website has done!"/>
             <HelpComponent.CollapsibleText title="How to pause a music" content="There will be an answer when the whole website has done!"/>
             <HelpComponent.CollapsibleText title="How to stop a music" content="There will be an answer when the whole website has done!"/>
             <HelpComponent.CollapsibleText title="How to skip a music" content="There will be an answer when the whole website has done!"/>
             <HelpComponent.CollapsibleText title="How to loop a music" content="There will be an answer when the whole website has done!"/>
-            <p className="CommonText">『Coming Soon』</p>
+            <p>Coming Soon</p>
             <HelpComponent.CollapsibleText title="Comming Soon" content="There will be an answer when the whole website has done!"/>
-            <p className="CommonText">『Bugs Report!』</p>
+            <p>Bugs Report!</p>
             <p><HelpComponent.TestComponent/></p>
         </div>
     );
