0396dc8b4286dbc3ea93c53978826fe9365424b2
BenjaminThio <84172516+BenjaminThio@users.noreply.github.com>
2021-05-22 14:32:22 +0800

    User Account :Added New UI Design & Special Functions!

--- src/components/UserAccount/Password.tsx
+++ src/components/UserAccount/Password.tsx
@@ -1,26 +1,31 @@
 import "./../../pages/UserAccount";
-import React, { useState } from 'react';
 
 const Password = (props: any) => {
     function visibility(event: any) {
-      var x = event.currentTarget.nextElementSibling;
-      if (x.type === "password") {
-        x.type = "text";
-      } else {
-        x.type = "password";
+      var Text = event.currentTarget.nextElementSibling;
+      Text.classList.toggle("PassVisibility");
+      if (Text.type === "text") {
+        Text.type = "password";
+      } 
+      else {
+        Text.type = "text";
       }
     }
     return (
-        <div>
+        <div className="ParagraphPosition">
             <h2>Change your password</h2>
+          <p>
             <label className="Label" htmlFor="CurrentPass">Current Password</label>
-            <button onClick={visibility}>Show</button>
-            <input id="CurrentPass" type="text" placeholder="Current Password"></input>
-            <label className="Label" htmlFor="NewPass">New Password</label>
-            <button onClick={visibility}>Show</button>
-            <input id="NewPass" type="text" placeholder="New Password"></input>
             <br/>
-            <button>UPDATE</button>
+            <button className="PassButton" onClick={visibility}>Show</button>
+            <input id="CurrentPass" type="text" placeholder="Current Password"></input>
+          </p>
+          <p className="ObjectPosition">
+            <label className="Label" htmlFor="NewPass">New Password</label>
+            <br/>
+            <button className="PassButton" onClick={visibility}>Show</button>
+            <input id="NewPass" type="text" placeholder="New Password"></input>      
+          </p>
         </div>
     );
 };
