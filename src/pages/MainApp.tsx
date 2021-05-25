2df339b46402d51eae6973114f33109afada9aac
Tiffceet <zkang.loo@gmail.com>
2021-05-25 13:40:20 +0800

    Frontend: remove additional code checks for auth

--- src/pages/MainApp.tsx
+++ src/pages/MainApp.tsx
@@ -7,16 +7,14 @@ function MainApp() {
     const { authed, loading } = AuthService();
     return loading ? (
         <div>Loading...</div>
-    ) : authed ? (
-        <div>
-            <PrivateRoute authed={true} path="/app" exact component={App.App} />
-            <PrivateRoute authed={true} path="/app/user" component={App.UserAccount} />
-            <PrivateRoute authed={true} path="/app/test" component={App.TestPage} />
-            <PrivateRoute authed={true} path="/app/linkdrive" component={App.LinkGDrive} />
-        </div>
     ) : (
-        <Redirect to="/" />
-    );
+        <div>
+            <PrivateRoute authed={authed} path="/app" exact component={App.App} />
+            <PrivateRoute authed={authed} path="/app/user" component={App.UserAccount} />
+            <PrivateRoute authed={authed} path="/app/test" component={App.TestPage} />
+            <PrivateRoute authed={authed} path="/app/linkdrive" component={App.LinkGDrive} />
+        </div>
+    )
 }
 
 export default MainApp;
