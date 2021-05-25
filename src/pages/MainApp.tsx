bb285c778f7c3810b17f0f6b90baba22aa08aca7
Tiffceet <zkang.loo@gmail.com>
2021-05-25 13:38:51 +0800

    Frontend: Auth update
    - All mainapp now kicks you back to home page is you are not logon

--- src/pages/MainApp.tsx
+++ src/pages/MainApp.tsx
@@ -1,18 +1,21 @@
 // import { Route } from "react-router-dom";
 import PrivateRoute from "../components/generic/PrivateRoute";
 import * as App from "./App";
-import {AuthService} from "../services/Auth";
+import { AuthService } from "../services/Auth";
+import { Redirect } from "react-router";
 function MainApp() {
-    const {authed, loading} = AuthService();
-    return (
+    const { authed, loading } = AuthService();
+    return loading ? (
+        <div>Loading...</div>
+    ) : authed ? (
         <div>
-            {/* <PrivateRoute authed={true} path="/app" exact component={App.App} />
+            <PrivateRoute authed={true} path="/app" exact component={App.App} />
             <PrivateRoute authed={true} path="/app/user" component={App.UserAccount} />
             <PrivateRoute authed={true} path="/app/test" component={App.TestPage} />
-            <PrivateRoute authed={true} path="/app/linkdrive" component={App.LinkGDrive} /> */}
-            {loading ? "isLoading" : "loading done!"}<br />
-            {`authed is ${authed}`}<br />
+            <PrivateRoute authed={true} path="/app/linkdrive" component={App.LinkGDrive} />
         </div>
+    ) : (
+        <Redirect to="/" />
     );
 }
 
