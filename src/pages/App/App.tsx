fb5c06d6af018b0b58be5614af3e8abe9663cf07
Tiffceet <zkang.loo@gmail.com>
2021-05-26 21:01:15 +0800

    App: Added toggle sidebar when in mobile

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import example_song_cover from "./../../images/example-song-cover.jpeg";
 import { useState } from "react";
 import * as AppSubPage from "./index";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {Route} from "react-router";
 
 function App() {
@@ -16,11 +17,20 @@ function App() {
                     navBarState={showNavBar}
                     setNavBar={setNavBarDisplay}
                 /> */}
-                <Route path="/app" exact component={() => <MainAppComponent.Content navBarState={showNavBar} setNavBar={setNavBarDisplay} />} />
-                <Route path="/app/user" component={AppSubPage.UserAccount} />
-                <Route path="/app/linkdrive" component={AppSubPage.LinkGDrive} />
-                <Route path="/app/test" component={AppSubPage.TestPage} />
-                <Route path="/app/organize" component={() => <AppSubPage.Organizer className="mainapp-content-container" /> } />
+                <div className="container-fluid">
+                    <div className="row d-md-none d-block">
+                        <div className="col" style={{color: "white", padding: "10px", cursor: "pointer"}} onClick={() => { setNavBarDisplay(!showNavBar) }}>
+                            <FontAwesomeIcon size="lg" icon="align-justify" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <Route path="/app" exact component={() => <MainAppComponent.Content navBarState={showNavBar} setNavBar={setNavBarDisplay} />} />
+                        <Route path="/app/user" component={AppSubPage.UserAccount} />
+                        <Route path="/app/linkdrive" component={AppSubPage.LinkGDrive} />
+                        <Route path="/app/test" component={AppSubPage.TestPage} />
+                        <Route path="/app/organize" component={() => <AppSubPage.Organizer className="mainapp-content-container" /> } />
+                    </div>
+                </div>
             </div>
             <div className="music-player">
                 <MainAppComponent.MusicPlayer song_cover={example_song_cover} />
