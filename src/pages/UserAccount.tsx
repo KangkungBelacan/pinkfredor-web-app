5761e8a95705c9732c48ff8b2a15d44c80e23561
BenjaminThio <84172516+BenjaminThio@users.noreply.github.com>
2021-05-26 19:28:31 +0800

    User Account: Fixed bugs

--- src/pages/UserAccount.tsx
+++ src/pages/UserAccount.tsx
@@ -4,44 +4,50 @@ import "./UserAccount.css";
 function UserAccount() {
     const link = "http://localhost:3000/"
     return (
-        <div>
+        <div className="core">
         <UserAccountComponent.Sidebar line1="Home" link1={link} line2="App" link2={link+"app"} line3="Help" link3={link+"help"} line4="About" link4={link+""}/>
-            <div className="CorePosition">
+            <div style={{marginLeft: "70px", marginTop: "-50px"}}>
                 <h1>Account Settings</h1>
                 <h2>Personal Information</h2>
-                <p>
-                    <label className="Label" htmlFor="FirstName">First Name</label>
-                    <br/>
-                    <input id="FirstName" type="text" placeholder="Your First Name"></input>
-                </p>
-                <p className="ObjectPosition">
-                    <label className="Label" htmlFor="LastName">Last Name</label>
-                    <br/>
-                    <input id="LastName" type="text" placeholder="Your Last Name"></input>
-                </p>
-                <p className="ParagraphPosition1">
-                    <label className="Label" htmlFor="Email">Email</label>
-                    <br/>
-                    <input id="Email" type="text" placeholder="Your Email"></input>
-                    <p><br/><button className="Button">UPDATE</button></p>
-                    <p><h2 className="HeaderLine">H2 Header here</h2></p>
-                </p>
-                <p>
-                    <UserAccountComponent.Password/>
-                </p>
-                <p className="ParagraphPosition1">
-                    <p><br/><button className="Button">UPDATE</button></p>
-                    <h2>Number Given In Store</h2>
-                    <label className="Label" htmlFor="PhoneNum">Phone Number <a className="LinkText" href={link+"help"}>ⓘ</a></label>
-                    <br/>
-                    <input id="PhoneNum" type="text" placeholder="Phone Number"></input>
-                    <br/>
-                    <label className="Label" htmlFor="ClubNum">Club Card Number</label>
-                    <br/>
-                    <input id="ClubNum" type="text" placeholder="Club Card Number"></input>
-                </p>
-                <p className="ParagraphPosition2">Need help?Please visit our <a href="#0">Contact Us</a> page!
-                <p><br/><button className="Button">UPDATE</button></p></p>
+                <div className="flex">
+                    <div>
+                        <label className="Label" htmlFor="FirstName">First Name</label><br/>
+                        <input id="FirstName" type="text" placeholder="Your First Name"></input>
+                    </div>
+                    <div>
+                        <label className="Label" htmlFor="LastName">Last Name</label><br/>
+                        <input id="LastName" type="text" placeholder="Your Last Name"></input>
+                    </div>                    
+                </div>
+                <div className="flex">
+                    <div>
+                        <label className="Label" htmlFor="Email">Email</label><br/>
+                        <input id="Email" type="text" placeholder="Your Email"></input>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div><button className="Button">UPDATE</button></div>
+                </div>
+                <UserAccountComponent.Password/>
+                <br/><h2>Number Given In Store</h2>
+                <div className="flex">
+                    <div>
+                        <label className="Label" htmlFor="PhoneNum">Phone Number <a className="LinkText" href={link+"help"}>ⓘ</a></label><br/>
+                        <input id="PhoneNum" type="text" placeholder="Phone Number"></input>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>
+                        <label className="Label" htmlFor="ClubNum">Club Card Number</label><br/>
+                        <input id="ClubNum" type="text" placeholder="Club Card Number"></input>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div>Need help?Please visit our <a href="">Contact Us</a> page!</div>
+                </div>
+                <div className="flex">
+                    <div><button className="Button">UPDATE</button></div>
+                </div>
             </div>
         </div>
     );
