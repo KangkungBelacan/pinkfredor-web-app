cce0192b747acba3f3eeeb93b57ca0c2039c1f62
Tiffceet <zkang.loo@gmail.com>
2021-06-02 11:48:00 +0800

    Add: Dark theme to material-table

--- src/pages/App/OrganizerSubPage/MaterialTableMUITheme.ts
+++ src/pages/App/OrganizerSubPage/MaterialTableMUITheme.ts
@@ -1,16 +1,7 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 const DARK_THEME = createMuiTheme({
     palette: {
-        text: {
-            primary: "#ffffff",
-            secondary: "#ffffff",
-            disabled: "#5f5f5f",
-            hint: "#2a2a2a",
-        },
-        background: {
-            paper: "rgb(51, 51, 51)",
-            default: "rgb(51, 51, 51)",
-        }
+        type: "dark"
     },
 });
 
