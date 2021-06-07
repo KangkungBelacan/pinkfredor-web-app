01eb02712526d284d5f19ae684f75b13d9c2fcae
Tiffceet <zkang.loo@gmail.com>
2021-06-07 22:24:36 +0800

    Add: b64ToBlobURL() generic function

--- src/global-imports.ts
+++ src/global-imports.ts
@@ -50,4 +50,15 @@ const useWindowSize = () => {
     return size;
 };
 
+const b64ToBlobURL = (b64Data: string, contentType: string) => {
+    const byteCharacters = atob(b64Data);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: contentType });
+    return URL.createObjectURL(blob);
+};
+
 export { axios, useAxiosPOST, useWindowSize };
