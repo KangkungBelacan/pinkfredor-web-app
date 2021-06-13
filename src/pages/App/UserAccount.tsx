4a3a0bff1c8676437b3cb3df420b53227c6bfbdd
Tiffceet <zkang.loo@gmail.com>
2021-06-13 19:45:16 +0800

    Get help

--- src/pages/App/UserAccount.tsx
+++ src/pages/App/UserAccount.tsx
@@ -2,7 +2,7 @@ import { CategoriesTopBarItemProps } from "../../interface/components/MainApp/Ca
 import CategoriesTopBar from "../../components/MainApp/CategoriesTopBar";
 import * as UserAccountComponent from "../../components/UserAccount";
 import { Route } from "react-router";
-import "./UserAccount.css"
+import "./UserAccount.css";
 
 function UserAccount() {
     let items: Array<CategoriesTopBarItemProps> = [
@@ -23,45 +23,101 @@ function UserAccount() {
         },
     ];
     return (
-            <div className="user-content-container">
-                <CategoriesTopBar items={items}/>
-                <div>
-                    <h1 className="Text" style={{margin:"10px"}}>Account Settings</h1>
-                    <h2 className="Text" style={{margin:"10px"}}>Personal Information</h2>
-                    <div className="flex">
-                        <div style={{margin:"10px"}}>
-                            <label className="Label" htmlFor="FirstName">First Name</label><br/>
-                            <input className="Input" id="FirstName" type="text" placeholder="Your First Name"></input>
-                        </div>
-                        <div style={{margin:"10px"}}>
-                            <label className="Label" htmlFor="LastName">Last Name</label><br/>
-                            <input className="Input" id="LastName" type="text" placeholder="Your Last Name"></input>
-                        </div>         
+        <div className="mainapp-content-container">
+            <CategoriesTopBar items={items} />
+            <div style={{height: "calc(85vh - 64px)", overflowY: "auto"}}>
+                <h1 className="Text" style={{ margin: "10px" }}>
+                    Account Settings
+                </h1>
+                <h2 className="Text" style={{ margin: "10px" }}>
+                    Personal Information
+                </h2>
+                <div className="flex">
+                    <div style={{ margin: "10px" }}>
+                        <label className="Label" htmlFor="FirstName">
+                            First Name
+                        </label>
+                        <br />
+                        <input
+                            className="Input"
+                            id="FirstName"
+                            type="text"
+                            placeholder="Your First Name"
+                        ></input>
                     </div>
-                    <div className="flex">
-                        <div style={{margin:"10px"}}>
-                            <label className="Label" htmlFor="Email">Email</label><br/>
-                            <input className="Input" id="Email" type="text" placeholder="Your Email"></input>
-                        </div>
+                    <div style={{ margin: "10px" }}>
+                        <label className="Label" htmlFor="LastName">
+                            Last Name
+                        </label>
+                        <br />
+                        <input
+                            className="Input"
+                            id="LastName"
+                            type="text"
+                            placeholder="Your Last Name"
+                        ></input>
                     </div>
-                    <div style={{margin:"10px"}}><button className="Button">UPDATE</button></div>
-                    <UserAccountComponent.Password/>
-                    <br/><h2 className="Text" style={{margin:"10px"}}>Number Given In Store</h2>
-                    <div className="flex">
-                        <div style={{margin:"10px"}}>
-                            <label className="Label" htmlFor="PhoneNum">Phone Number <a className="LinkText" href={"/help"}>ⓘ</a></label><br/>
-                            <input className="Input" id="PhoneNum" type="text" placeholder="Phone Number"></input>
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div style={{margin:"10px"}}>
-                            <label className="Label" htmlFor="ClubNum">Club Card Number</label><br/>
-                            <input className="Input" id="ClubNum" type="text" placeholder="Club Card Number"></input>
-                        </div>
-                    </div>
-                    <div>Need help?Please visit our <a href="">Contact Us</a> page!</div>
-                    <div style={{margin:"10px"}}><button className="Button">UPDATE</button></div>
                 </div>
+                <div className="flex">
+                    <div style={{ margin: "10px" }}>
+                        <label className="Label" htmlFor="Email">
+                            Email
+                        </label>
+                        <br />
+                        <input
+                            className="Input"
+                            id="Email"
+                            type="text"
+                            placeholder="Your Email"
+                        ></input>
+                    </div>
+                </div>
+                <div style={{ margin: "10px" }}>
+                    <button className="Button">UPDATE</button>
+                </div>
+                <UserAccountComponent.Password />
+                <br />
+                <h2 className="Text" style={{ margin: "10px" }}>
+                    Number Given In Store
+                </h2>
+                <div className="flex">
+                    <div style={{ margin: "10px" }}>
+                        <label className="Label" htmlFor="PhoneNum">
+                            Phone Number{" "}
+                            <a className="LinkText" href={"/help"}>
+                                ⓘ
+                            </a>
+                        </label>
+                        <br />
+                        <input
+                            className="Input"
+                            id="PhoneNum"
+                            type="text"
+                            placeholder="Phone Number"
+                        ></input>
+                    </div>
+                </div>
+                <div className="flex">
+                    <div style={{ margin: "10px" }}>
+                        <label className="Label" htmlFor="ClubNum">
+                            Club Card Number
+                        </label>
+                        <br />
+                        <input
+                            className="Input"
+                            id="ClubNum"
+                            type="text"
+                            placeholder="Club Card Number"
+                        ></input>
+                    </div>
+                </div>
+                <div>
+                    Need help?Please visit our <a href="">Contact Us</a> page!
+                </div>
+                <div style={{ margin: "10px" }}>
+                    <button className="Button">UPDATE</button>
+                </div>
+            </div>
         </div>
     );
 }
