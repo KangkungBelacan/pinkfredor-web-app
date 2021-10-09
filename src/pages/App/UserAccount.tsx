77a0739394d10e964361545ae3751712007d5c0c
Tiffceet <zkang.loo@gmail.com>
2021-10-09 12:09:56 +0800

    Add: New user account page WIP

--- src/pages/App/UserAccount.tsx
+++ src/pages/App/UserAccount.tsx
@@ -1,30 +1,53 @@
-import { CategoriesTopBarItemProps } from "../../interface/components/MainApp/CategoriesTopBarItemProps";
-import CategoriesTopBar from "../../components/MainApp/CategoriesTopBar";
 // import * as UserAccountComponent from "../../components/UserAccount";
 import "./UserAccount.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function UserAccount() {
-    let items: Array<CategoriesTopBarItemProps> = [
-        {
-            display_text: "Info",
-            faIconClass: "info-circle",
-            link: "/app/user/info",
-        },
-        {
-            display_text: "Settings",
-            faIconClass: "user-cog",
-            link: "/app/user/settings",
-        },
-        {
-            display_text: "Google Drive",
-            faIconClass: ["fab", "google-drive"],
-            link: "/app/linkdrive",
-        },
-    ];
     return (
         <div className="mainapp-content-container">
-            <div style={{height: "85vh", overflowY: "auto"}}>
-                
+            <div
+                style={{
+                    height: "85vh",
+                    overflowY: "auto",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <div className="user-settings-container">
+                    <div
+                        style={{
+                            backgroundColor: "#242424",
+                            borderRadius: "0.5rem 0 0 0.5rem",
+                        }}
+                    >
+                        <div className="user-settings-menu-item-container">
+                            <div></div>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        height: "4px",
+                                        backgroundColor: "white",
+                                        width: "4px",
+                                        borderRadius: "4px",
+                                        transition: "height .25s",
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="user-settings-menu-item">
+                                <FontAwesomeIcon
+                                    icon="address-book"
+                                    size="lg"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div style={{ margin: "10px" }}>Content Here</div>
+                </div>
             </div>
         </div>
     );
