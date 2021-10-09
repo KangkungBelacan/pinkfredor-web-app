bfc7ad2af6dd8afc0dd2b9a5790dadf1ba497f96
Tiffceet <zkang.loo@gmail.com>
2021-10-09 11:21:06 +0800

    Remove old user settings page

--- src/components/UserAccount/index.ts
+++ src/components/UserAccount/index.ts
@@ -1,1 +1,1 @@
-export {default as Password} from "./Password";
+export {}
--- src/pages/App/UserAccount.tsx
+++ src/pages/App/UserAccount.tsx
@@ -1,6 +1,6 @@
 import { CategoriesTopBarItemProps } from "../../interface/components/MainApp/CategoriesTopBarItemProps";
 import CategoriesTopBar from "../../components/MainApp/CategoriesTopBar";
-import * as UserAccountComponent from "../../components/UserAccount";
+// import * as UserAccountComponent from "../../components/UserAccount";
 import "./UserAccount.css";
 
 function UserAccount() {
@@ -24,97 +24,7 @@ function UserAccount() {
     return (
         <div className="mainapp-content-container">
             <div style={{height: "85vh", overflowY: "auto"}}>
-                <h1 className="Text" style={{ margin: "10px" }}>
-                    Account Settings
-                </h1>
-                <h2 className="Text" style={{ margin: "10px" }}>
-                    Personal Information
-                </h2>
-                <div className="flex">
-                    <div style={{ margin: "10px" }}>
-                        <label className="Label" htmlFor="FirstName">
-                            First Name
-                        </label>
-                        <br />
-                        <input
-                            className="Input"
-                            id="FirstName"
-                            type="text"
-                            placeholder="Your First Name"
-                        ></input>
-                    </div>
-                    <div style={{ margin: "10px" }}>
-                        <label className="Label" htmlFor="LastName">
-                            Last Name
-                        </label>
-                        <br />
-                        <input
-                            className="Input"
-                            id="LastName"
-                            type="text"
-                            placeholder="Your Last Name"
-                        ></input>
-                    </div>
-                </div>
-                <div className="flex">
-                    <div style={{ margin: "10px" }}>
-                        <label className="Label" htmlFor="Email">
-                            Email
-                        </label>
-                        <br />
-                        <input
-                            className="Input"
-                            id="Email"
-                            type="text"
-                            placeholder="Your Email"
-                        ></input>
-                    </div>
-                </div>
-                <div style={{ margin: "10px" }}>
-                    <button className="Button">UPDATE</button>
-                </div>
-                <UserAccountComponent.Password />
-                <br />
-                <h2 className="Text" style={{ margin: "10px" }}>
-                    Number Given In Store
-                </h2>
-                <div className="flex">
-                    <div style={{ margin: "10px" }}>
-                        <label className="Label" htmlFor="PhoneNum">
-                            Phone Number{" "}
-                            <a className="LinkText" href={"/help"}>
-                                ⓘ
-                            </a>
-                        </label>
-                        <br />
-                        <input
-                            className="Input"
-                            id="PhoneNum"
-                            type="text"
-                            placeholder="Phone Number"
-                        ></input>
-                    </div>
-                </div>
-                <div className="flex">
-                    <div style={{ margin: "10px" }}>
-                        <label className="Label" htmlFor="ClubNum">
-                            Club Card Number
-                        </label>
-                        <br />
-                        <input
-                            className="Input"
-                            id="ClubNum"
-                            type="text"
-                            placeholder="Club Card Number"
-                        ></input>
-                    </div>
-                </div>
-                <div className="Text">
-                    Need help?Please visit our <a href="/help">Contact Us</a> page!
-                </div>
-                <div style={{ margin: "10px" }}>
-                    <button className="Button">UPDATE</button>
-                </div>
+                
             </div>
         </div>
     );
