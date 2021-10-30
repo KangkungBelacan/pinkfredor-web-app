79300ee323f598fc083f5b1e72620b838e95d514
Tiffceet <zkang.loo@gmail.com>
2021-10-30 16:51:42 +0800

    Fix: VolumeBar now gradually change volume

--- src/components/MainApp/MusicPlayerSubComponent/VolumeBar.tsx
+++ src/components/MainApp/MusicPlayerSubComponent/VolumeBar.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 
-const VolumeBar = (props:any) => {
+const VolumeBar = (props: any) => {
     const [volumeDisplay, setVolumeDisplay] = useState(100);
-    const setVolumeOnReleased = (evt: any) => {
-        let newVolume = evt.currentTarget.value;
-        if(isNaN(newVolume)) {
-            return;
-        }
-        props.setVolume(parseInt(newVolume));
-    };
-    return <input type="range" min='0' max='100' defaultValue={volumeDisplay} onMouseUp={setVolumeOnReleased} onChange={(evt) => {setVolumeDisplay(parseInt(evt.target.value))}} className="player-misc-controls-volume-slider" style={{ backgroundSize: (volumeDisplay - 0) * 100 / (100 - 0) + '% 100%' }} />
+    return (
+        <input
+            type="range"
+            min="0"
+            max="100"
+            defaultValue={volumeDisplay}
+            onChange={(evt) => {
+                props.setVolume(parseInt(evt.target.value));
+                setVolumeDisplay(parseInt(evt.target.value));
+            }}
+            className="player-misc-controls-volume-slider"
+            style={{
+                backgroundSize:
+                    ((volumeDisplay - 0) * 100) / (100 - 0) + "% 100%",
+            }}
+        />
+    );
 };
 
 export default VolumeBar;
