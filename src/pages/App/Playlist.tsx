af815f00bec6b2124c3ea43d800e3aeecc213f53
Tiffceet <zkang.loo@gmail.com>
2021-10-31 12:19:19 +0800

    Update Playlist.tsx

--- src/pages/App/Playlist.tsx
+++ src/pages/App/Playlist.tsx
@@ -1,11 +1,18 @@
+import useAxios from "axios-hooks";
 import { useParams } from "react-router-dom";
 const Playlist = () => {
     const { playlist_id }: any = useParams();
-    return (
-        <div className="mainapp-content-container">
-           Playlist stuff here
-        </div>
-    );
+    const [{ data: pl_data, loading: pl_loading, error: pl_err }, _p] =
+        useAxios(
+            { url: `/api/indexes/playlist/${playlist_id}`, method: "GET" },
+            { useCache: false }
+        );
+    const [{ data: filesData, loading: filesLoading, error: filesErr }, _f] =
+        useAxios(
+            { url: "/api/indexes/files", method: "GET" },
+            { useCache: false }
+        );
+    return <div className="mainapp-content-container">Playlist stuff here</div>;
 };
 
 export default Playlist;
